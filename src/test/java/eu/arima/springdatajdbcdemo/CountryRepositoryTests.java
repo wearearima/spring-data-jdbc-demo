cf888e3b0ff6fb6a1a08dbddf1ca0722654f0352
Ugaitz Urien <ugaitz@arima.eu>
2019-09-16 12:30:19 +0200

    Update commits

--- src/test/java/eu/arima/springdatajdbcdemo/CountryRepositoryTests.java
+++ src/test/java/eu/arima/springdatajdbcdemo/CountryRepositoryTests.java
@@ -2,18 +2,24 @@ package eu.arima.springdatajdbcdemo;
 
 import eu.arima.springdatajdbcdemo.city.City;
 import eu.arima.springdatajdbcdemo.country.Country;
+import eu.arima.springdatajdbcdemo.country.CountryPageableRepository;
 import eu.arima.springdatajdbcdemo.country.CountryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.internal.util.collections.Sets;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.stream.StreamSupport;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJdbcTest
@@ -21,6 +27,10 @@ public class CountryRepositoryTests {
 
     @Autowired
     CountryRepository countryRepository;
+    @Autowired
+    CountryPageableRepository countryPageableRepository;
+
+    Logger LOGGER = LoggerFactory.getLogger(CountryRepositoryTests.class);
 
     @Test
     public void findAllReturnsEmpty() {
@@ -29,20 +39,52 @@ public class CountryRepositoryTests {
     }
 
     @Test
-    public void saveACountry() {
+    public void reloadModel() {
         final Country country = createCountryModel();
         countryRepository.save(country);
-        final Optional<Country> savedCountry = countryRepository.findById(country.getId());
-        System.out.println(savedCountry);
+        final Optional<Country> loaded = countryRepository.findById(country.getId());
+        assertFalse(country==loaded.get());
     }
 
     @Test
-    public void updateACountry() {
+    public void notLazyLoad() {
+        final Country country = createCountryModel();
+        countryRepository.save(country);
+        final Optional<Country> loaded = countryRepository.findById(country.getId());
+        LOGGER.info("En los logs se ve que ya se ha ejecutado la consulta de la relación");
+        assertEquals(3, loaded.get().getCities().size());
+    }
+
+    @Test
+    public void update() {
+        Country country = createCountryBasicModel();
+        countryRepository.save(country);
+        country.setPopulation(10);
+        countryRepository.save(country);
+        LOGGER.info("La query de actualizar se ha ejecutado");
+    }
+    @Test
+    public void updateWithRelations() {
         Country country = createCountryModel();
         countryRepository.save(country);
         country.setPopulation(10);
         countryRepository.save(country);
-        System.out.println(country);
+        LOGGER.info("Se ha ejecutado tanto la actualización de la tabla Country, como los delete e inserts de city");
+    }
+
+    @Test
+    public void findPage() {
+        Country country = createCountryBasicModel();
+        countryRepository.save(country);
+        country.setId(null);
+        countryRepository.save(country);
+        final PageRequest firstPageRequest = PageRequest.of(0, 10);
+        try {
+            final Page<Country> firstPage = countryPageableRepository.findAll(firstPageRequest);
+            fail();
+        } catch (Exception e) {
+            LOGGER.info("Spring Data JDBC no implementa pageable");
+        }
     }
 
     private Country createCountryModel() {
@@ -50,6 +92,10 @@ public class CountryRepositoryTests {
         City bilbo = new City("Bilbao", 345821);
         City madrid = new City("Madrid", 3174000);
         return new Country("Spain", 46720000, Sets.newSet(madrid, donosti, bilbo));
+    }
+
+    private Country createCountryBasicModel() {
+        return new Country("Spain", 46720000, new HashSet<>());
     }
 
 }
